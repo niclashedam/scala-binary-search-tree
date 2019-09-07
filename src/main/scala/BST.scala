9b6acf73c9cfd84167da58901afc34a88eb54611
Niclas Hedam <niclas@hed.am>
2019-09-07 22:54:18 +0200

    min(), max()

--- src/main/scala/BST.scala
+++ src/main/scala/BST.scala
@@ -12,18 +12,27 @@ class BST[K, V](implicit ord: Ordering[K]) {
 
   private var root: Tree[K, V] = Leaf
 
-  def size(): Int = {
-    size(this.root)
+  def find(k: K): Option[V] = {
+    find(root, k)
   }
 
   def insert[A](k: K, v: V): Unit = {
     this.root = insert(root, k, v);
   }
 
-  def find(k: K): Option[V] = {
-    find(root, k)
+  def size(): Int = {
+    size(this.root)
   }
 
+  def min(): Option[V] = {
+      min(this.root)
+  }
+
+  def max(): Option[V] = {
+      max(this.root)
+  }
+
+  @annotation.tailrec
   private def find(t: Tree[K, V], key: K): Option[V] = t match {
     case Leaf => None
     case Branch(l, r, k, v) =>
@@ -43,5 +52,21 @@ class BST[K, V](implicit ord: Ordering[K]) {
   private def size(t: Tree[K, V]): Int = t match {
     case Leaf               => 0
     case Branch(l, r, _, _) => 1 + size(l) + size(r)
+  }
+
+  private def min(t: Tree[K, V]): Option[V] = t match {
+    case Branch(l, _, _, v) => l match {
+        case Leaf => Some(v)
+        case Branch(_,_,_,_) => min(l)
+    }
+    case Leaf => None
+  }
+
+  private def max(t: Tree[K, V]): Option[V] = t match {
+    case Branch(_, r, _, v) => r match {
+        case Leaf => Some(v)
+        case Branch(_,_,_,_) => max(r)
+    }
+    case Leaf => None
   }
 }
--- src/test/scala/BSTTest.scala
+++ src/test/scala/BSTTest.scala
@@ -14,16 +14,41 @@ class BSTTest extends FlatSpec {
     assert(bst.size() === 3)
   }
 
-  "The BST" should "be able to find inserted keys (order them correctly)" in {
+  it should "be able to find inserted keys (order them correctly)" in {
     val bst = new BST[String, Int]
 
     bst.insert("foo", 8)
     bst.insert("bar", 2)
     bst.insert("baz", 6)
+    bst.insert("qux", -3)
+    bst.insert("quux", 12)
+    bst.insert("corge", 5)
 
     assert(bst.find("foo") === Some(8))
     assert(bst.find("bar") === Some(2))
     assert(bst.find("baz") === Some(6))
-    assert(bst.find("quux") === None)
+    assert(bst.find("qux") === Some(-3))
+    assert(bst.find("quux") === Some(12))
+    assert(bst.find("corge") === Some(5))
+  }
+
+  it should "be return None if key is not found" in {
+    val bst = new BST[String, Int]
+
+    assert(bst.find("foo") === None)
+  }
+
+  it should "be able to find the minimum and maximum value" in {
+    val bst = new BST[String, Int]
+
+    bst.insert("foo", 8)
+    bst.insert("bar", 2)
+    bst.insert("baz", 6)
+    bst.insert("qux", -3)
+    bst.insert("quux", 12)
+    bst.insert("corge", 5)
+
+    assert(bst.max() === Some(-3)) // qux
+    assert(bst.min() === Some(2)) // bar
   }
 }
